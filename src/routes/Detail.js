ed03f491b9f73871daa67a8e0cc1abf0b80fc104
petitesize <petitesize22@gmail.com>
2025-02-06 15:45:27 +0900

    Detail 화면에서의 삭제 구현

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { actionCreators } from "../store";
 
 function Detail() {
   const { id } = useParams();
   const toDos = useSelector((state) => state);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
-
-  console.log(toDo);
+  const onDelete = () => {
+    dispatch(actionCreators.deleteTodo(id));
+    navigate("/");
+  };
   return (
     <>
       <h1>{toDo?.text}</h1>
       <h5>Created at: {toDo?.id}</h5>
       {/* delete 만들기 */}
       {/* 로컬스토리지에 state 저장, 새로고침해도 남아있도록 */}
+      <button onClick={onDelete}>X</button>
     </>
   );
 }
